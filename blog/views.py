a833d595925112c40e12f88b88b20884f1167104
Franco Bayugar <francobayugardev@gmail.com>
2024-02-17 15:32:15 -0300

    templates updated

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'Franco Bayugar',
+        'title': 'Blog Post #1',
+        'content': 'Post #1 content',
+        'date_posted': '17-Feb-24'
+    },
+    {
+        'author': 'Dbaggio Regal',
+        'title': 'Blog Post #2',
+        'content': 'Post #2 content',
+        'date_posted': '18-Feb-24'
+    },
+]
+    
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
