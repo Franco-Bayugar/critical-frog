14c8f082086874d97ed496fa4c156f8147207e08
Franco Bayugar <francobayugardev@gmail.com>
2024-04-01 08:19:11 -0300

    created update user info

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,30 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
+
+# Creation of user
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    
+        
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+
+#* This sections is to update info
+# allow to update username and email
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+        
+    class Meta:
+        model = User
+        fields = ['username', 'email']   
+        
+# allow to the image        
+class ProfileUpdateForm(forms.ModelForm):
+    class meta:
+        model = Profile
+        field = ['image']
+        
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 
